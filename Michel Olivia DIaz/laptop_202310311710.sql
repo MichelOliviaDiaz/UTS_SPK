f5cd61cd544f9f7f83e212d2276284c4e6430c59
MichelOliviaDiaz <149291214+MichelOliviaDiaz@users.noreply.github.com>
2023-10-31 17:15:58 +0700

    Add files via upload

---
+++ Michel Olivia DIaz/laptop_202310311710.sql
@@ -0,0 +1,11 @@
+INSERT INTO michel.laptop (id,nama_produk,harga_per_unit,processor,ram,ukuran_layar,"storage") VALUES
+	 ('1','Acer Nitro 5',10999000,'Intel Core i5-12500H',8,'15-inch',512),
+	 ('2','Acer Swift X (SFX14-41G)',13649000,'Ryzen 5 5600U',16,'14-inch',512),
+	 ('3','Acer Aspire 5 Slim A514-54G',6548000,'Intel Core i3-1115G4',8,'14-inch',512),
+	 ('4','Acer Aspire Vero',9650000,'Intel Core-15 3.2GHz',8,'15.6-inch',512),
+	 ('5','Acer Spin 5',13349000,'Intel Core i3-1115G4',8,'14-inch',512),
+	 ('6','Acer Aspire Slim 3 A314-36M-36ZH',6955000,'Intel Core i3-N305 3.80GHz',4,'11.6-inch',500),
+	 ('7','Acer Spin 1 SP111-33',5589000,'Intel Celeron 1.80GHz',4,'11.6-inch',500),
+	 ('8','Acer Nitro 5 AN515-58-78PT',17199000,'Intel Core i7-12700H',8,'15.6-inch',512),
+	 ('9','Acer Chromebook 14',4899000,'Intel Celeron 1.80GHz',4,'14-inch',32),
+	 ('10','Acer Nitro 5 AN515-58-710Q',20499000,'Intel Core i7-12650H',16,'15.6-inch',512);
